e3193ef44f6652593bae13e8ff69f751ef783ccb
Henrique dos Santos <hsantos124.br@gmail.com>
2026-05-25 08:53:01 -0300

    B

--- API101/backend/server.js
+++ API101/backend/server.js
@@ -1,8 +1,14 @@
 import express from "express"
+import cors from "cors"
 import initRoutes from "./src/routes/routes.js"
 
 const app = express()
 const port = 8080
+
+app.use(cors({
+    origin : "http://localhost:5500"
+}))
+
 initRoutes(app)
 app.get("/", (req, res) => {
     res.send("The API is working!")
